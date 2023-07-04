ae4482d67f2495bcb997e386b6358e92df030af1
Pedro <Pedrowiskibr@gmail.com>
2023-07-04 12:42:42 -0300

    Criado a A pagina de login, e responsiva

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Logo from 'C:/Users/User/Desktop/projeto/src/assets/LogoPaginaLogin.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="loginContainer">
+        <img src={Logo} alt=""/>
+        <h2 id="title"><strong>Entre em Sua Conta</strong></h2>
+        <form id="form1">
+          <label id="label1" htmlFor="email">E-mail</label> <br />
+          <input type="email" className='input' name="email" /> <br />
+
+          <label id="label2" htmlFor="password">Senha</label><br />
+          <input type="password" className='input' name="password"  /><br />
+        
+          <button id="button1" type="submit">Entrar</button><br />
+        </form>
+      </div>
+      <div className="registerContainer">
+        
+        <h2 id="title2">Novo Aqui?</h2>
+        <p>Registre-se e venha fazer parte da maior rede social!</p>
+        <button id="button2">Registrar-se</button>
+
+      </div>
     </div>
   );
 }
